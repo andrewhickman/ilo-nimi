accad18fab040cd94af05c694c62906b271f8fba
Andrew Hickman <andrew.hickman1@sky.com>
2024-04-07 01:43:21 +0100

    wuwojiti

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,9 @@ fn main() {
 }
 
 fn name(rng: &mut impl Rng) -> String {
-    let len = [1, 2, 2, 2, 2, 3, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 6].choose(rng).unwrap();
+    let len = [1, 2, 2, 2, 2, 3, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 6]
+        .choose(rng)
+        .unwrap();
 
     let mut name = String::new();
     initial(&mut name, rng);
@@ -32,18 +34,20 @@ fn initial(buf: &mut String, rng: &mut impl Rng) {
 
 fn syllable(buf: &mut String, rng: &mut impl Rng) {
     let last = buf.chars().last();
-    let onset = ['p', 't', 'k', 's', 'm', 'n', 'l', 'j', 'w'].choose_weighted(rng, |v| match v {
-        'p' => 61,
-        't' => 45,
-        'k' => 91,
-        's' => 64,
-        'm' if last != Some('n') => 50,
-        'n' if last != Some('n') => 32,
-        'l' => 83,
-        'j' => 35,
-        'w' => 34,
-        _ => 0,
-    }).unwrap();
+    let onset = ['p', 't', 'k', 's', 'm', 'n', 'l', 'j', 'w']
+        .choose_weighted(rng, |v| match v {
+            'p' => 61,
+            't' => 45,
+            'k' => 91,
+            's' => 64,
+            'm' if last != Some('n') => 50,
+            'n' if last != Some('n') => 32,
+            'l' => 83,
+            'j' => 35,
+            'w' => 34,
+            _ => 0,
+        })
+        .unwrap();
     buf.push(*onset);
 
     nucleus(buf, rng);
@@ -51,14 +55,16 @@ fn syllable(buf: &mut String, rng: &mut impl Rng) {
 
 fn nucleus(buf: &mut String, rng: &mut impl Rng) {
     let last = buf.chars().last();
-    let nucleus = ['a', 'i', 'e', 'o', 'u'].choose_weighted(rng, |v| match v {
-        'a' => 146,
-        'e' => 94,
-        'i' if last != Some('t') => 109,
-        'o' if last != Some('w') => 82,
-        'u' if last != Some('w') => 60,
-        _ => 0,
-    }).unwrap();
+    let nucleus = ['a', 'i', 'e', 'o', 'u']
+        .choose_weighted(rng, |v| match v {
+            'a' => 146,
+            'e' => 94,
+            'i' if last != Some('t') && last != Some('j') => 109,
+            'o' if last != Some('w') => 82,
+            'u' if last != Some('w') => 60,
+            _ => 0,
+        })
+        .unwrap();
 
     buf.push(*nucleus);
 
